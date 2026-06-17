5dcb89595b98ebf60606909dac6f104d7b4d5d3a
AlobarQuest <devon.watkins@gmail.com>
2026-06-17 10:16:55 -0400

    style: hoist read_guard.core import to top of test module (E402)

--- tests/test_read_guard.py
+++ tests/test_read_guard.py
@@ -1,6 +1,7 @@
 import re
 import uuid
 from security_scan import token_shapes
+from security_scan.read_guard import core
 
 
 def _synth_token() -> str:
@@ -15,9 +16,6 @@ def test_bws_token_rx_matches_synthetic_token():
 def test_bws_token_rx_ignores_lookalikes():
     for s in ["0.1.2", str(uuid.uuid4()), "abc123def456", "0.short.x"]:
         assert token_shapes.BWS_TOKEN_RX.search(s) is None
-
-
-from security_scan.read_guard import core
 
 
 def test_scan_finds_token_in_plain_text():
